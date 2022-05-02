c2cbae17ace2ef4e36a843ca0056c0dc7a7b45e6
London <londonperry23@gmail.com>
2022-05-02 14:38:29 -0600

    initial config

--- frontend/site/src/lib/sanityClient.js
+++ frontend/site/src/lib/sanityClient.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import sanityClient from '@sanity/client';
 
 const config = {
-  projectId: process.env.SANITY_PROJECT_ID || '8hj1t7km',
+  projectId: process.env.SANITY_PROJECT_ID || 'rtdmluxk',
   dataset: process.env.SANITY_DATASET || 'production',
   token: process.env.SANITY_READ_TOKEN,
   apiVersion: process.env.SANITY_API_VERSION || 'v1',
