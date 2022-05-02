5705e464652bfe0cea8688b07097cc6969e822d2
London Perry <55641618+londonperry@users.noreply.github.com>
2022-05-02 13:55:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rocket-sanity-minimal-starter
---
+++ frontend/site/src/styles/baseStyles.js
@@ -0,0 +1,9 @@
+import { css } from 'lit';
+
+export const baseStyles = css`
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: Jost, Arial, Helvetica, sans-serif;
+  }
+`;
